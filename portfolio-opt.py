5ef620aff6acf4a2e931c5d39256aaaf91f91689
Luis Fernando Rabelo Nogueira <luis.nogueira@petros.com.br>
2022-04-01 15:04:34 -0300

    Adicionando a matriz de covariância EWMA correta e uma função que transforma a matriz de covariância para a matriz de correlação

--- portfolio-opt.py
+++ portfolio-opt.py
@@ -1,4 +1,3 @@
-from more_itertools import sample
 import pandas as pd
 import numpy as np
 import pandas_datareader as pdr
@@ -34,16 +33,17 @@ def simple_expected_returns(df):
     returns = []
     for column in df.columns:
         returns.append((1 + np.mean(df[column])) ** (252) - 1)
-    return returns
-
+    return returns    
+    
 def sample_covariance_matrix(df):
     return df.cov()
 
-def ewma_weights(num, Lambda):
+def ewma_weights(num, Lambda = 0.94):
     lst_ewma_weights = []
     for index in range(0, num):
         lst_ewma_weights.append((1-Lambda) * (Lambda ** index))
     lst_ewma_weights = [ewma_weight / sum(lst_ewma_weights) for ewma_weight in lst_ewma_weights]
+    lst_ewma_weights = lst_ewma_weights[::-1]
     return lst_ewma_weights
 
 def ewma_volatility_rolling(df, 
@@ -54,22 +54,38 @@ def ewma_volatility_rolling(df,
     dfc = df.copy().reset_index(drop=True)
     n_asset = len(dfc.columns)
     for column in dfc.columns:
-        dfc[f'U^2 {column}'] = dfc[column] ** 2
+        dfc[column] = dfc[column] ** 2
     lst_ewma_weights = ewma_weights(rolling, Lambda)
     lst_ewma_weights = lst_ewma_weights[::-1]
     for column in dfc.columns[0:n_asset]:
         for row in range(rolling, len(dfc)):
             dfc.loc[row, f'EWMA Vol. {column}'] = (np.sqrt((np.transpose(np.array(lst_ewma_weights))
-                                                @ np.array(dfc[f'U^2 {column}'].iloc[row-rolling:row])) * freq))
-    return dfc[dfc.columns[n_asset * 2:]].dropna().set_index(dfr.index[rolling:])
+                                                @ np.array(dfc[column].iloc[row-rolling:row])) * freq))
+    return dfc[dfc.columns[n_asset:]].dropna().set_index(dfr.index[rolling:])
 
-# Acho que estou fazendo invertido, procurar saber a multiplicação matricial via Excel
-
-def ewma_covariance_matrix(df, Lambda = 0.94):
+def ewma_covariance_matrix(df, 
+                           Lambda = 0.94,
+                           annualize = False):
     dfc = df.copy()
-    lst_ewma_weights = ewma_weights(len(dfc), Lambda)
-    ret_ewma = dfc.mul(lst_ewma_weights, axis= 0 )
-    return np.transpose(ret_ewma) @ dfc
+    name_assets = df.columns
+    dfc['EWMA'] = ewma_weights(len(dfc), Lambda)
+    cov_matrix = pd.DataFrame(columns=name_assets, index=name_assets)
+    for num, column in enumerate(name_assets):
+        for num2, column2 in enumerate(name_assets):
+            if column == column2:
+                # Variance EWMA = series(EWMA_w) * (series(returns) - mean(returns)) ** 2
+                cov_matrix[column].iloc[num2] = np.sum(dfc['EWMA'] * (dfc[column] - np.mean(dfc[column])) ** 2)
+            else:
+                # Covariance EWMA = series(EWMA_w) * (series(returns_x) - mean(returns_x)) * (series(returns_y) - mean(returns_y))
+                cov_matrix[column].iloc[num2] = np.sum(dfc['EWMA'] * (dfc[column] - np.mean(dfc[column])) * (dfc[column2] - np.mean(dfc[column2])))
+    if annualize == True:
+        cov_matrix = cov_matrix * 252
+    return cov_matrix
 
-ewma_covariance_matrix(dfr).corr()
-dfr.corr()
+def cov_to_corr(cov_matrix):
+    cov_matrix = cov_matrix.astype(float)
+    Dinv = np.diag(1 / np.sqrt(np.diag(cov_matrix)))
+    corr = np.dot(Dinv, np.dot(cov_matrix, Dinv))
+    return pd.DataFrame(corr, index=cov_matrix.index, columns=cov_matrix.index)
+
+y = ewma_covariance_matrix(dfr)
